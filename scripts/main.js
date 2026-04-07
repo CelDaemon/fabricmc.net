7b897436e085caab3c4331ff434fb9ea4aa425ea
CelDaemon <celdaemon@voidgroup.net>
2026-04-07 13:23:07 +0000

    Deploying to static from  @ de65bf0ea739a3979f3cfe6f7ccc5c6b0fb7ead3 🚀

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+const s = (e, o) => {
+  const t = e[o];
+  return t ? typeof t == "function" ? t() : Promise.resolve(t) : new Promise((i, r) => {
+    (typeof queueMicrotask == "function" ? queueMicrotask : setTimeout)(r.bind(null, new Error("Unknown variable dynamic import: " + o)));
+  });
+};
+function n(e) {
+  return () => s(/* @__PURE__ */ Object.assign({ "./lib/Documentation.svelte": () => import("./Documentation.5dfc5817.js"), "./lib/DownloadIcon.svelte": () => import("./DownloadIcon.39c279f6.js"), "./lib/Installer.svelte": () => import("./Installer.09258292.js"), "./lib/LegacyVersion.svelte": () => import("./LegacyVersion.3c493d7c.js"), "./lib/MCUpdater.svelte": () => import("./MCUpdater.680b64d5.js"), "./lib/Server.svelte": () => import("./Server.acff9dc7.js"), "./lib/Technic.svelte": () => import("./Technic.0498ff83.js"), "./lib/Template.svelte": () => import("./Template.49db5eab.js"), "./lib/Versions.svelte": () => import("./Versions.7f08db20.js") }), `./lib/${e}.svelte`);
+}
+const c = n("Installer"), l = n("MCUpdater"), a = n("Technic"), m = n("Server"), p = n("Versions"), u = n("Documentation"), v = n("Template");
+let b = {
+  Installer: c,
+  MCUpdater: l,
+  Technic: a,
+  Server: m,
+  Versions: p,
+  Documentation: u,
+  Template: v
+};
+function d() {
+  for (const e of document.getElementsByClassName("fabric-component")) {
+    if (!(e instanceof HTMLElement))
+      continue;
+    const o = e.dataset.component;
+    if (!o)
+      throw new Error("Missing data-component attribute");
+    const t = b[o];
+    if (!t)
+      throw new Error("Unknown component: " + o);
+    t().then((i) => {
+      new i.default({
+        target: e
+      });
+    });
+  }
+}
+document.addEventListener("DOMContentLoaded", d);
